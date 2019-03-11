0155691634a2463c73be88db8f0541c4546f2eb3
changmmm <changmeimei92@hotmail.com>
2019-03-11 13:11:11 +0800

    Add files via upload

--- R/mutAnnotate.R
+++ R/mutAnnotate.R
@@ -1,20 +1,16 @@
 #' Hotspots annotation.
 #'
 #' @param hotspots.file Hotspots to be annotated tsv file.
-#' @param cds.file Coding regions bed file, default file = Ensembl75.CDS.bed.
 #' @param promoter.file Promoter regions bed file, default file = Ensembl75.promoters.coding.bed.
 #' @param utr3.file 3'UTR regions bed file, default file = Ensembl75.3UTR.coding.bed.
 #' @param utr5.file 5'UTR regions bed file, default file = Ensembl75.5UTR.coding.bed.
-#' @param intron.file Intron regions bed file, default file = Ensembl75.intron.coding.bed.
 #' @param other.annotations Text file containing URLs of additional regions to be annotated, default = NULL.
 #' @return Annotated hotspots.
 #' @export
 
-mutAnnotate = function(hotspots.file, cds.file = system.file("extdata", "Ensembl75.CDS.bed", package = "MutSpot"), 
-                       promoter.file = system.file("extdata", "Ensembl75.promoters.coding.bed", package = "MutSpot"),
+mutAnnotate = function(hotspots.file, promoter.file = system.file("extdata", "Ensembl75.promoters.coding.bed", package = "MutSpot"),
                        utr3.file = system.file("extdata", "Ensembl75.3UTR.coding.bed", package = "MutSpot"), 
                        utr5.file = system.file("extdata", "Ensembl75.5UTR.coding.bed", package = "MutSpot"), 
-                       intron.file = system.file("extdata", "Ensembl75.intron.bed", package = "MutSpot"),
                        other.annotations = NULL) {
   
   hotspots.ann = read.delim(hotspots.file, header = TRUE, stringsAsFactors = FALSE)
@@ -23,31 +19,31 @@ mutAnnotate = function(hotspots.file, cds.file = system.file("extdata", "Ensembl
   mut.regions <- with(hotspots.ann, GenomicRanges::GRanges(chrom, IRanges::IRanges(start, end), pval = pval, length = length, p.bg = p.bg, k = k, fdr = fdr))
   names(mut.regions) = rownames(hotspots.ann)
   
-  roi.cds <- bed.to.grangeslist(cds.file)
-  roi.transcripts = range(roi.cds)
-  roi.transcripts = BiocGenerics::unlist(roi.transcripts)
-  roi.transcripts = roi.transcripts + 10000
+  # roi.cds <- bed.to.grangeslist(cds.file)
+  # roi.transcripts = range(roi.cds)
+  # roi.transcripts = BiocGenerics::unlist(roi.transcripts)
+  # roi.transcripts = roi.transcripts + 10000
   
   roi.prom <- bed.to.grangeslist(promoter.file)
   roi.3utr <- bed.to.grangeslist(utr3.file)
   roi.5utr <- bed.to.grangeslist(utr5.file)
-  roi.intron <- bed.to.grangeslist(intron.file)
+  # roi.intron <- bed.to.grangeslist(intron.file)
 
   # Gene hits
-  gene.hits = IRanges::findOverlaps(mut.regions, roi.transcripts)
-  if (length(gene.hits) != 0) {
-    
-  gene.hits.m = S4Vectors::as.matrix(gene.hits)
-  gene.names = data.frame(gene.hits.m, mut_region = names(mut.regions[gene.hits.m[ ,"queryHits"]]), transcript_id = names(roi.transcripts[gene.hits.m[ ,"subjectHits"]]))
-  gene.names2 = aggregate(transcript_id ~ mut_region, FUN=paste, collapse = "/", data = gene.names)
-  
-  } else { 
-    
-    gene.names2 = data.frame(mut_region = hotspots.ann$mut_region, transcript_id = NA)
-    
-  }
-  gene.names2$mut_region = as.character(gene.names2$mut_region)
-  colnames(gene.names2)[2] = "gene"
+  # gene.hits = IRanges::findOverlaps(mut.regions, roi.transcripts)
+  # if (length(gene.hits) != 0) {
+  #   
+  # gene.hits.m = S4Vectors::as.matrix(gene.hits)
+  # gene.names = data.frame(gene.hits.m, mut_region = names(mut.regions[gene.hits.m[ ,"queryHits"]]), transcript_id = names(roi.transcripts[gene.hits.m[ ,"subjectHits"]]))
+  # gene.names2 = aggregate(transcript_id ~ mut_region, FUN=paste, collapse = "/", data = gene.names)
+  # 
+  # } else { 
+  #   
+  #   gene.names2 = data.frame(mut_region = hotspots.ann$mut_region, transcript_id = NA)
+  #   
+  # }
+  # gene.names2$mut_region = as.character(gene.names2$mut_region)
+  # colnames(gene.names2)[2] = "gene"
   
   # Promoter hits
   prom.hits = IRanges::findOverlaps(mut.regions, roi.prom)
@@ -98,22 +94,23 @@ mutAnnotate = function(hotspots.file, cds.file = system.file("extdata", "Ensembl
   colnames(utr5.names2)[2] = "5UTR"
   
   # Intron hits  
-  intron.hits = IRanges::findOverlaps(mut.regions, roi.intron)
-  if (length(intron.hits) != 0) {
-
-  intron.hits.m = S4Vectors::as.matrix(intron.hits)
-  intron.names = data.frame(intron.hits.m, mut_region = names(mut.regions[intron.hits.m[ ,"queryHits"]]), transcript_id = names(roi.intron[intron.hits.m[ ,"subjectHits"]]))
-  intron.names2 = aggregate(transcript_id ~ mut_region, FUN = paste, collapse = ";", data = intron.names)
+  # intron.hits = IRanges::findOverlaps(mut.regions, roi.intron)
+  # if (length(intron.hits) != 0) {
+  # 
+  # intron.hits.m = S4Vectors::as.matrix(intron.hits)
+  # intron.names = data.frame(intron.hits.m, mut_region = names(mut.regions[intron.hits.m[ ,"queryHits"]]), transcript_id = names(roi.intron[intron.hits.m[ ,"subjectHits"]]))
+  # intron.names2 = aggregate(transcript_id ~ mut_region, FUN = paste, collapse = ";", data = intron.names)
+  # 
+  # } else {
+  #   
+  #   intron.names2 = data.frame(mut_regions = hotspots.ann$mut_region, transcript_id = NA)
+  #   
+  # } 
+  # intron.names2$mut_region = as.character(intron.names2$mut_region)
+  # colnames(intron.names2)[2] = "intron"
   
-  } else {
-    
-    intron.names2 = data.frame(mut_regions = hotspots.ann$mut_region, transcript_id = NA)
-    
-  } 
-  intron.names2$mut_region = as.character(intron.names2$mut_region)
-  colnames(intron.names2)[2] = "intron"
-  
-  names.list = list(hotspots.ann, gene.names2, prom.names2, utr3.names2, utr5.names2, intron.names2)
+  # names.list = list(hotspots.ann, gene.names2, prom.names2, utr3.names2, utr5.names2, intron.names2)
+  names.list = list(hotspots.ann, prom.names2, utr3.names2, utr5.names2)
   df.out = merged.data.frame = Reduce(function(...) merge(..., by = "mut_region", all.x = T), names.list)
 
   if (!is.null(other.annotations)) {
