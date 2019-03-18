ef43ee6d3a872619d749a19f44145842b6630154
changmmm <changmeimei92@hotmail.com>
2019-03-18 10:17:08 +0800

    Add files via upload

--- MutSpot_Rpackage/R/subtract.regions.from.roi.chr.R
+++ MutSpot_Rpackage/R/subtract.regions.from.roi.chr.R
@@ -10,8 +10,8 @@ subtract.regions.from.roi.chr <- function(rois, sub,chr = 'chr10') {
   
   if (class(rois) == "GRangesList") {
     
-    roi.chr = BiocGenerics::unlist(rois[as.character(GenomicRanges::seqnames(rois)) == chr]); sub.chr = BiocGenerics::unlist(sub[as.character(GenomicRanges::seqnames(sub)) == chr]);
-    GenomicRanges::elementMetadata(roi.chr) <- NULL; GenomicRanges::elementMetadata(sub.chr) <- NULL
+    roi.chr = BiocGenerics::unlist(rois[as.character(seqnames(rois)) == chr]); sub.chr = BiocGenerics::unlist(sub[as.character(seqnames(sub)) == chr]);
+    elementMetadata(roi.chr) <- NULL; elementMetadata(sub.chr) <- NULL
     
     # Return if one list is empty
     if (length(roi.chr) == 0 || length(sub.chr) == 0) {
@@ -24,24 +24,25 @@ subtract.regions.from.roi.chr <- function(rois, sub,chr = 'chr10') {
     }
     
     r1 = c(roi.chr, sub.chr)
-    r2 = IRanges::disjoin(r1)
+    r2 = disjoin(r1)
     
     ## Problem here is that setdiff merges adjacent ranges ...
     #r3 = setdiff(r2,sub.chr)
     
-    r3 = r2[-S4Vectors::queryHits(IRanges::findOverlaps(r2, sub.chr))]
-    rovl = IRanges::findOverlaps(rois, r3)
-    r4 = tapply(r3[S4Vectors::subjectHits(rovl)], S4Vectors::queryHits(rovl), function(xs) {IRanges::reduce(BiocGenerics::unlist(xs))})
-    r4.names = tapply(names(rois[S4Vectors::queryHits(rovl)]), S4Vectors::queryHits(rovl), function(x) x[1])
+    r3 = r2[queryHits(findOverlaps(r2, sub.chr))]
+    rovl = findOverlaps(rois, r3)
+    r4 = tapply(r3[subjectHits(rovl)], queryHits(rovl), function(xs) { reduce(BiocGenerics::unlist(xs)) })
+    r4.names = tapply(names(rois[queryHits(rovl)]), queryHits(rovl), function(x) x[1])
     names(r4) = r4.names
-    rl = GenomicRanges::GRangesList(r4);
+    rl = GRangesList(r4);
     rl
     
   } else if (class(rois)=="GRanges") {
     
-    roi.chr = rois[GenomicRanges::seqnames(rois) == chr]; sub.chr = sub[GenomicRanges::seqnames(sub) == chr];
-    GenomicRanges::elementMetadata(roi.chr) <- NULL; GenomicRanges::elementMetadata(sub.chr) <- NULL
-    # return if one list is empty
+    roi.chr = rois[seqnames(rois) == chr]; sub.chr = sub[seqnames(sub) == chr];
+    elementMetadata(roi.chr) <- NULL; elementMetadata(sub.chr) <- NULL
+    
+    # Return if one list is empty
     if (length(roi.chr) == 0 || length(sub.chr) == 0) {
       
       return(roi.chr)
@@ -49,12 +50,12 @@ subtract.regions.from.roi.chr <- function(rois, sub,chr = 'chr10') {
     }
     
     r1 = c(roi.chr,sub.chr)
-    r2 = GenomicRanges::disjoin(r1)
+    r2 = disjoin(r1)
     
     ## Problem here is that setdiff merges adjacent ranges ...
     #r3 = setdiff(r2,sub.chr)
     
-    r3 = r2[-S4Vectors::queryHits(IRanges::findOverlaps(r2, sub.chr))]
+    r3 = r2[-queryHits(findOverlaps(r2, sub.chr))]
     r3
     
   }
